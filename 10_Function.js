0bcda88c2e50878c699cee7b8ebc69c15fefb0c8
vijay dankhara <vijaydankhara111@gmail.com>
2023-12-10 21:55:54 +0530

    10-12-2023

---
+++ 10_Function.js
@@ -0,0 +1,46 @@
+// Function
+
+
+// Hello(); // functio call
+
+// function Hello()   // function define
+// {
+//     console.log('Hello Surat How Are You');
+// }
+
+
+// function Add(a,b)
+// {
+//     console.log(a*b);
+// }
+
+
+// Add(10,'Hello');
+
+// function Product(a,b,c)
+// {
+//     return a*b*c;
+// }
+
+// let x = Product(10,20,30);
+// console.log(x);    // output ->  6000
+
+// console.log(Product(1,20,5));       // output ->  100
+
+
+
+//////////////////////////////////////////////////
+
+// function as Variable /anonymous Function
+// const Product = Function (x,y,z)
+// {
+//     return x*y*z ;
+// }
+
+// console.log(Product(11,22,33));
+
+let skill = Function()
+{
+    console.log('JavaSript - is most popular language');
+};
+skill();
