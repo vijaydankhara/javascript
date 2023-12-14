bcec59c148cad76aec43a68b3b5c0791d6e61c51
vijay dankhara <vijaydankhara111@gmail.com>
2023-12-14 08:00:20 +0530

    14-12-2023

---
+++ 12_Array_Method.js
@@ -0,0 +1,121 @@
+/* 
+===================================> || Array methodes || <===========================================
+
+1.> lenth()       7.> splice()          13.> Array Max()    19.> reduseRight()      25.> includes()   
+2.> push()        8.> slice()           14.> Array Min()    20.> every()            26.> copyWithin() 
+3.> pop()         9.> sort()            15.> forEach()      21.> some()
+4.> unShift()    10.> Reverce  ()       16.> map()          22.> find()
+5.> Shift()      11.> changing ()       17.> filter()       23.> findIndex()
+6.> Merging()    12.> Numeric Sort:-()  18.> reduse()       24.> entries ()
+*/
+
+
+// 1.> lenth of array
+// const a=[11,22,33,44,55,66,77,88,99];
+// console.log("Lenth of Array is :- ",a);
+// /////////////////////////////////////////
+
+// 2.> array push    ==> add element of last position
+// const a = [11,22,33,44,55];
+// a.push(111,222);
+// console.log(a);
+////////////////////////
+
+// 3.> array pop   ==> delet element of last position
+// const a = [11,22,33,44,55];
+// a.pop();
+// console.log(a);
+//////////////////////////////////////////////////////
+
+// 4.> Array unShift:-       ==> add element of frist position
+// const a = [11,22,33,44,55];
+// a.unshift(111,222);
+// console.log(a);
+//////////////////////////////////////////////////
+
+// 5.> Array Shift:-  ==> delet element of frist position
+// const a = [11,22,33,44,55];
+// a.shift();
+// console.log(a);
+/////////////////////////////////////////////////////
+
+// 6.>  Array Merging(Concatenating):-   ==>  murge array
+// const bat = ['Gill','Rohit','kohli','Surya','Rinku','KL Rahul'];
+// const bow = ['Hardik','Jadeja','Kuldeep','Siraj','Shami'];
+// const all = bat.concat(bow);
+// console.log(all);
+//////////////////////////////////////////////////////
+
+// 7.>  Array splice   ==>add element of any position in array
+// const bat = ['Gill','Rohit','kohli','Surya','Rinku','KL Rahul'];
+// bat.splice(3,0,"MS Dhoni","Hardik");
+// console.log(bat);
+////////////////////////////////////////////////////////////
+
+// 8.>  Array slice   ==> Delete element of any position in array
+// const bat = ['Gill','Rohit','kohli','Surya','Rinku','KL Rahul'];
+// const team = bat.slice(1,3);
+// console.log(bat);
+// console.log(team);
+/////////////////////////////////////////////////////////////
+
+// 9.> Array sort  ==> shorting Element
+// const alfa = ['c','d','e','a','b'];
+// alfa.sort();
+// console.log(alfa);
+/////////////////////////////////////////////
+
+// 10.> Array Reverce  ==> shorting Element
+// const alfa = ['c','d','e','a','b'];
+// alfa.sort();
+// console.log(alfa.reverse());
+
+///////////////////////////////////////////////////////////////////////
+
+// 11.> changing    
+
+///////////////////////////////////////////////////////////////////////
+// 12.> Numeric Sort:-   
+
+
+////////////////////////////////////////////////////////////
+// 13.>  Array Max ():-  ==> finde the array in Highet(Big)Number.
+
+///////////////////////////////////////////////////////////
+// 14.>  Array Min ():-  ==> finde the array in lowest (Small) Number.
+
+////////////////////////////////////////////////////
+// 15.> Array forEach ():- ==> 
+
+//////////////////////////////////////////////////////
+// 16.> Array Map ():-   ==>  5 * 2 =10
+
+//////////////////////////////////////////////////
+//  17.> Array filter ():-
+
+/////////////////////////////////////////////////////////
+// 18.> Array reduse ():-
+
+/////////////////////////////////////////////////////////
+// 19.> Array reduseRight ():-
+
+/////////////////////////////////////////////////////////
+// 20.> Array every():-
+
+/////////////////////////////////////////////////////////
+// 21.> Array some():-
+
+/////////////////////////////////////////////////////////
+// 22.> Array find():-
+
+/////////////////////////////////////////////////////////
+// 23.> Array findIndex():-
+
+/////////////////////////////////////////////////////////
+// 24.> Array entries():-
+
+/////////////////////////////////////////////////////////
+// 25.> Array includes():-
+
+/////////////////////////////////////////////////////////
+// 26.> Array copyWithin():-
