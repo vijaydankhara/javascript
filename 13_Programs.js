0b68743ada0adf06f28fa86bae8fba9a7930b130
vijay dankhara <vijaydankhara111@gmail.com>
2023-12-16 09:16:37 +0530

    16-12-2023

---
+++ 13_Programs.js
@@ -0,0 +1,72 @@
+// 1. Write a JS function that reverse a 15438 number.  
+// const reverce =[1,5,4,3,8];
+// reverce.sort;
+// console.log(reverce.reverse());   // this is output 83451
+
+///////////////////////////////
+
+
+
+// 2. Write a JS function that returns a passed string with letters in alphabetical order. ==> 'webmaster'
+// function arr(str)
+// {
+// return str.split('').sort().join('')
+// }
+// console.log(arr("webmaster"));
+
+
+
+/////////////////////////////////////////////////////
+// 3. Write a JS function that accepts a string as a parameter and converts the first letter
+//    of each word of the string in upper case. ==> 'the quick brown fox'
+
+// function fristlatercap(str)
+// {
+// let data = str.split(' ');  
+// for (let i = 0; i < data.length; i++)
+// {
+//     data[i] = data[i][0].toUpperCase()+data[i].substring(1);  
+// }
+// let fristlater = data.join(' ');
+// return fristlater;
+// }
+
+// const enterstring = 'the quick brown fox';
+// const output = fristlatercap(enterstring);
+// console.log(output);
+
+///////////////////////////////////////////////////////////
+4.
+// function findLongestWord(str) {
+//     let words = str.split(' ');
+//     let longestWord = '';
+  
+//     for (let i = 0; i < words.length; i++) {
+//       if (words[i].length > longestWord.length) {
+//         longestWord = words[i];
+//       }
+//     }
+  
+//     return longestWord;
+//   }
+//   let sentence = 'Web Development Tutorial';
+//   let longest = findLongestWord(sentence);
+//   console.log(longest); // Output: 'Development'
+
+
+//////////////////////////////////////////
+// 5. Write a JavaScript function that checks whether a number is perfect.
+// function isPerfectNumber(number) {
+//     let sum = 0;
+    
+//     for (let i = 1; i < number; i++) {
+//       if (number % i === 0) {
+//         sum += i;
+//       }
+//     }
+    
+//     return sum === number;
+//   }
+//   console.log(isPerfectNumber(6)); // Output: true
+//   console.log(isPerfectNumber(28)); // Output: true
+//   console.log(isPerfectNumber(12)); // Output: false
