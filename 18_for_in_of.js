eeadeceac5978239a85cd8393d35276efff9a6ca
vijay dankhara <vijaydankhara111@gmail.com>
2023-12-21 09:37:02 +0530

    21-12-2023

---
+++ 18_for_in_of.js
@@ -0,0 +1,59 @@
+// for in loop and for of loop
+
+// let person = {
+//     name: "Hardik Pandya",
+//     team: "Mumbai",
+//     postion: "Captan",
+//     Salary: "17.50CR",
+//     age: 28,
+//     email: "Hardik@33.in"
+// };
+
+// let txt = "";
+// for(let i in person){
+//     txt += person[i] + "\n";
+// }
+
+// console.log(txt);
+
+
+
+
+////////////////////////////////////
+
+
+
+
+// let person = [11,22,33,44,55];
+
+// let txt = "";
+
+// for(let i of person){
+//     txt += i + "\t";
+// }
+// console.log(txt);
+
+
+
+//////////////////////////////////////////////////////
+
+// ... rest parameter / spread operator
+
+let person = {
+    name: "Hardik Pandya",
+    team: "Mumbai",
+    postion: "Captan",
+   
+    age: 28,
+    email: "Hardik@33.in"
+};
+  
+  let data = person;
+//   let data = { ...person };
+  
+  person.salary = 17.50;
+  
+  person.playing = ["Mumbai Indian","Gujarat Titan"]
+  data.hobbies = ["Playing"];
+  console.log(person);
+  console.log(data);
